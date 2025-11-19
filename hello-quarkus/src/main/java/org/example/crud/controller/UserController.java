fa947ed8d388b14feb30f84c76adda1cbacb14e2
Marcelo Urban <mvurban@mvurban.eti.br>
2025-11-19 14:28:16 +0000

    Criando o primeiro get do crud

---
+++ hello-quarkus/src/main/java/org/example/crud/controller/UserController.java
@@ -0,0 +1,20 @@
+package org.example.crud.controller;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+
+   @GET
+   public Response test() {
+      return Response.ok("teste2").build();
+   }
+
+}
